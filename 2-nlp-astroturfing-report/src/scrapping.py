dd5b24add6928a684fc3047cbff691cf3ba7b2ea
CaballeroRAR <arq_caballero@outlook.com>
2026-02-26 11:32:03 -0600

    feat: creation of scrapping script

---
+++ 2-nlp-astroturfing-report/src/scrapping.py
@@ -0,0 +1,9 @@
+import praw
+
+reddit = praw.Reddit(
+    client_id="CLIENT_ID",
+    client_secret="CLIENT_SECRET",
+    password="PASSWORD",
+    user_agent="Comment Extraction (by u/USERNAME)",
+    username="USERNAME",
+)
