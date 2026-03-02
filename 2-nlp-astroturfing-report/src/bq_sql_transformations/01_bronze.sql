5fecff38264d281e95567ae67501c42ad227b7bf
CaballeroRAR <arq_caballero@outlook.com>
2026-03-02 12:05:38 -0600

    feat: Change from view to table for better readability in GCP

--- 2-nlp-astroturfing-report/src/bq_sql_transformations/01_bronze.sql
+++ 2-nlp-astroturfing-report/src/bq_sql_transformations/01_bronze.sql
@@ -1,4 +1,4 @@
--- Create a bronze view exposing the raw ingested data without modifications
-CREATE OR REPLACE VIEW `{{project_id}}.{{dataset_id}}.bronze` AS
+-- Create a bronze table exposing the raw ingested data without modifications
+CREATE OR REPLACE TABLE `{{project_id}}.{{dataset_id}}.bronze` AS
 SELECT *
 FROM `{{project_id}}.{{dataset_id}}.comments_structured`;
