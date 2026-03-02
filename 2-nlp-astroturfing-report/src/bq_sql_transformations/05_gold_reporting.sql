9045c55593c8a01a58d9ad61c218f3e301069717
CaballeroRAR <arq_caballero@outlook.com>
2026-03-02 15:26:23 -0600

    feat: Introduce gold reporting view and visualization module for comprehensive astroturfing analysis.

---
+++ 2-nlp-astroturfing-report/src/bq_sql_transformations/05_gold_reporting.sql
@@ -0,0 +1,26 @@
+-- Create gold reporting view to join all insights for Tableau/BI tools
+CREATE OR REPLACE VIEW `{{project_id}}.{{dataset_id}}.gold_reporting_view` AS
+SELECT 
+    c.submission_id,
+    c.comment_id,
+    c.parent_id,
+    c.author_name,
+    c.body,
+    c.score AS comment_score,
+    c.controversiality,
+    c.created_utc AS comment_created_utc,
+    a.trust_score,
+    a.author_is_deleted,
+    a.author_created_utc,
+    a.author_comment_karma,
+    a.author_post_karma,
+    n.cluster_id,
+    n.sentiment_label,
+    n.sentiment_score,
+    n.umap_x,
+    n.umap_y
+FROM `{{project_id}}.{{dataset_id}}.silver_comments_clean` c
+LEFT JOIN `{{project_id}}.{{dataset_id}}.gold_author_profiles` a
+    ON c.author_name = a.author_name
+LEFT JOIN `{{project_id}}.{{dataset_id}}.gold_nlp_results` n
+    ON c.comment_id = n.comment_id;
